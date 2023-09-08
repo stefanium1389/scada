f4f179b555a50a799f67d61999263f960920f611
KristinaAndrijin <96635332+KristinaAndrijin@users.noreply.github.com>
2023-09-08 22:41:37 +0200

    alarms

--- front/scada-front/src/app/app-routing.module.ts
+++ front/scada-front/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { TrendingComponent } from './trending/trending.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { TagsManagementComponent } from './tags-management/tags-management.component';
+import { AlarmsComponent } from './tags-management/alarms/alarms.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
   {path: "trending", component: TrendingComponent},
   {path: "register", component: RegistrationComponent},
   {path: "tags", component: TagsManagementComponent},
+  {path: 'alarms', component: AlarmsComponent}
 ];
 
 @NgModule({
--- front/scada-front/src/app/tags-management/alarms/alarms.component.ts
+++ front/scada-front/src/app/tags-management/alarms/alarms.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-alarms',
@@ -7,9 +9,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlarmsComponent implements OnInit {
 
-  constructor() { }
+  selectedLegendItem!: string;
+  selectedItemsLow: string[] = [];
+  selectedItemsMedium: string[] = [];
+  selectedItemsHigh: string[] = [];
+
+  priority: string = "";
+  priorities: string[] = ['Low', 'Medium', 'High'];
+  type: string="";
+  types: string [] = ['Above', 'Below'];
+  limit: number = 0;
+  unit: string = 'C';
+  name: string = '';
+
+  alarmForm!: FormGroup;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+      console.log(this.name);
+      this.unit = params['unit'];
+      console.log(this.unit);
+    });
+    // for (let i = -15; i < 30; i++) {
+    //   this.selectedItemsLow.push(i.toString());
+    // }
+
+    // for (let i = 0; i < 60; i++) {
+    //   this.selectedItemsMedium.push(i.toString());
+    // }
+
+    // for (let i = 61; i < 100; i++) {
+    //   this.selectedItemsHigh.push(i.toString());
+    // }
+
+    this.alarmForm = new FormGroup({
+      priority: new FormControl('', Validators.required),
+      type: new FormControl('', Validators.required),
+      limit: new FormControl('', Validators.required),
+      btn: new FormControl("")
+    });
+  }
+
+  selectItemInLegend(event:any){
+    const clicked = event.target as HTMLDivElement;
+    // this.lastSelectedLegendItem = this.selectedLegendItem;
+    this.selectedLegendItem = clicked.innerText;
+    console.log(this.selectedLegendItem);
+    // this.updateCharts();
+  }
+
+  delete_alarm_low(item: string) {
+    const index = this.selectedItemsLow.indexOf(item);
+    if (index !== -1) {
+      this.selectedItemsLow.splice(index, 1);
+    }
+  }
+
+  delete_alarm_medium(item: string) {
+    const index = this.selectedItemsMedium.indexOf(item);
+    if (index !== -1) {
+      this.selectedItemsMedium.splice(index, 1);
+    }
+  }
+
+  delete_alarm_high(item: string) {
+    const index = this.selectedItemsHigh.indexOf(item);
+    if (index !== -1) {
+      this.selectedItemsHigh.splice(index, 1);
+    }
+  }
+  
+
+  edit_tag(obj: any) {
+    console.log('kris');
+  }
+
+  onSubmit() {
+    let priority = this.alarmForm.get('priority')?.value;
+    let type = this.alarmForm.get('type')?.value
+    let limit = this.alarmForm.get('limit')?.value
+    let sign = '';
+    if (type == 'Above') {
+      sign = '>';
+    } else {
+      sign = '<';
+    }
+    if (priority == 'Low') {
+      this.selectedItemsLow.push(sign + ' ' + limit + ' ' + this.unit);
+    } else if (priority == 'Medium') {
+      this.selectedItemsMedium.push(sign + ' ' + limit + ' ' + this.unit);
+    } else {
+      this.selectedItemsHigh.push(sign + ' ' + limit + ' ' + this.unit);
+    }
+    console.log(this.priority, this.type, this.limit);
   }
 
 }
