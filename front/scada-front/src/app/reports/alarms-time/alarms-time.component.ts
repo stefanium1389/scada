4f1574ba0dd54e2846c2dbc030dbb51bb3f156d0
VladimirGolosin <96448683+VladimirGolosin@users.noreply.github.com>
2023-09-16 14:27:43 +0200

    alarmi po vremenu skoro podešeni na frontu

--- front/scada-front/src/app/reports/alarms-time/alarms-time.component.ts
+++ front/scada-front/src/app/reports/alarms-time/alarms-time.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
+import { ReportService } from 'src/app/services/report.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-alarms-time',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alarms-time.component.css']
 })
 export class AlarmsTimeComponent implements OnInit {
+  startDate: string = '';
+  endDate: string = '';
+  dataSource = new MatTableDataSource<AlarmReportItem>(ELEMENT_DATA);
+  displayedColumns: string[] = ['time', 'priority'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor() { }
+  constructor(private reportService: ReportService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
+  processReport() {
+    if (this.startDate > this.endDate) {
+      alert('Start date must be before end date.');
+      return;
+    }
+  }
+
+}
+
+const ELEMENT_DATA: AlarmReportItem[] = [{time:"123",priority:"400"}];
+
+interface AlarmReportItem {
+  time: string;
+  priority: string;
 }
